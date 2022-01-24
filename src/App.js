f573093abec1ecfbdab41638e657de09157d40af
nelsonh88 <nelson.huezo88@gmail.com>
2022-01-24 18:34:01 -0500

    Creates components folder with component files to build the hangman App     - Created a static version with the components and now need to start inputting logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header></Header>
+      <div className="game-container">
+        <Figure></Figure>
+        <WrongLetters></WrongLetters>
+        <Word></Word>
+      </div>
+    </div>
+  );
 }
 
 export default App;
